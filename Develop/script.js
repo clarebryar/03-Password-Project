d5e0339ca55171918055f1ccca78c723dced1737
clarebryar <128009509+clarebryar@users.noreply.github.com>
2023-04-18 12:38:48 -0500

    redefined variables to use in for loop

--- Develop/script.js
+++ Develop/script.js
@@ -3,15 +3,26 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var characterOptions = ["!#$%&'()*+,-./:;<=>?@^_`{|}~"];
-var numericOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+var characters = "!#$%&()*+-./:;<=>?@^_`{|}~";
+console.log(characters.length);
+var characterSelect = [Math.floor(Math.random() * characters.length)];
 
+var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numericSelect = [Math.floor(Math.random() * numeric.length)];
+
+var smallAlph = "abcdefghijklmnop";
+var upperAlph = smallAlph.toUpperCase();
+
+var smallAlphSelect = [Math.floor(Math.random() * smallAlph.length)];
+var upperAlphSelect = [Math.floor(Math.random() * upperAlph)] 
 
 var passwordText = document.querySelector("#password");
 passwordText.value = password;
 
 // Write password to the #password input
+
 function writePassword() {
+  
   //Collecting users password length, and make sure it is between 8-128 characters
   var passwordLength = window.prompt(
     `How many characters long would you like your password to be?`
@@ -19,10 +30,11 @@ function writePassword() {
   console.log(passwordLength);
   {
     if (passwordLength < 8 || passwordLength > 128) {
-      window.alert("Password must be between 8-128 characters"); 
-        writePassword();
+      window.alert("Password must be between 8-128 characters");
+      writePassword(); 
+      }
     }
-  }
+  
 
   window.alert(
     "Your password must contain one of the following character types: lowercase, uppercase, numeric, and/or special characters, answer yes or no to the following questions for which character type(s) should be included in the password"
@@ -46,10 +58,9 @@ function writePassword() {
     "Should the password contain a special character?"
   );
   if (characters == "yes") console.log("include special character");
-}
-
-var password = generatePassword();
-function generatePassword() {
-
-
+  characters = "includecharacters";
+  var password = generatePassword();
+  function generatePassword() {
+   for (var i = 0; i < passwordLength; i * passwordLength);
+  }
 }
